cdb5541bbd8f6dfa5abfae9755662a113384fdcd
Kondrac22 <leonardo.yferreira@hotmail.com>
2025-07-18 22:08:37 -0300

    Set validator/serializer and register fastifyswagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+
 
 const app = fastify()
 
-app.register(fastifyCors, { origin: '*'})
+app.setValidatorCompiler(validatorCompiler) //seta o zod para validar os dados de entrada
+app.setSerializerCompiler(serializerCompiler) //seta o zod para serialização dos dados de saida
 
-app.listen({ port: 3333}).then(() => {
+app.register(fastifyCors, { origin: '*'}) // permite que qualquer domínio acesse a API
+
+app.register(fastifySwagger, {    // registra o swagger que cria uma pagina e usa a versao mais atual openapi
+    openapi: {
+        info: {
+            title: 'type API',
+            version: '1.0.0',
+        }
+    }
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',       // verificar rota final
+})
+ 
+app.listen({ port: 3333}).then(() => {  //criação do servidor porta 3333
     console.log("HTTP server running!")
 })
